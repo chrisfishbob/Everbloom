370a2eed7ed6f950e328aab7c20be1e0edbf40ed
Chris Hsu <56414801+chrisfishbob@users.noreply.github.com>
2023-08-29 20:06:19 -0700

    Update palette.lua

--- lua/everblush/palette.lua
+++ lua/everblush/palette.lua
@@ -1,10 +1,10 @@
 local palette = {
-    color0 = '#232a2d',
-    color1 = '#e57474',
-    color2 = '#8ccf7e',
-    color3 = '#e5c76b',
-    color4 = '#67b0e8',
-    color5 = '#c47fd5',
+    color0 = '#ffffff',
+    color1 = '#ffffff',
+    color2 = '#ffffff',
+    color3 = '#ffffff',
+    color4 = '#ffffff',
+    color5 = '#ffffff',
     color6 = '#6cbfbf',
     color7 = '#b3b9b8', -- identifiers
     color8 = '#2d3437',
@@ -18,8 +18,8 @@ local palette = {
     comment = '#ff0080',
     except = "#d446db",
     contrast = '#161d1f',
-    background = '#ffffff',
-    foreground = '#ffffff',
+    background = '#141b1e',
+    foreground = '#dadada',
     cursorline = '2c3333',
     none = 'NONE',
 }
